c3273222458006b00900e66e05a189e39802a133
p4535992 <tentimarco0@gmail.com>
2022-04-11 23:00:16 +0200

    added changelog and conflict

--- src/init.ts
+++ src/init.ts
@@ -18,6 +18,7 @@ import registerContextMenuHook from './modules/ContextMenu';
 import Identification from './modules/Identification';
 import registerItemClassMethod from './modules/Item';
 import CONSTANTS from './modules/constants';
+import API from './modules/api';
 
 /* ------------------------------------ */
 /* Initialize module					*/
@@ -40,6 +41,8 @@ Hooks.once('setup', () => {
   window.ForienIdentification = Identification;
 
   Hooks.callAll(`${CONSTANTS.MODULE_NAME}:afterSetup`);
+
+  setApi(API);
 });
 
 /* ------------------------------------ */
@@ -54,3 +57,58 @@ Hooks.once('ready', () => {
   Hooks.callAll(`${CONSTANTS.MODULE_NAME}:afterReady`);
 });
 
+// Add any additional hooks if necessary
+
+export interface ForienUnidentifiedItemModuleData {
+  api: typeof API;
+  socket: any;
+}
+
+/**
+ * Initialization helper, to set API.
+ * @param api to set to game module.
+ */
+export function setApi(api: typeof API): void {
+  const data = game.modules.get(CONSTANTS.MODULE_NAME) as unknown as ForienUnidentifiedItemModuleData;
+  data.api = api;
+}
+
+/**
+ * Returns the set API.
+ * @returns Api from games module.
+ */
+export function getApi(): typeof API {
+  const data = game.modules.get(CONSTANTS.MODULE_NAME) as unknown as ForienUnidentifiedItemModuleData;
+  return data.api;
+}
+
+/**
+ * Initialization helper, to set Socket.
+ * @param socket to set to game module.
+ */
+export function setSocket(socket: any): void {
+  const data = game.modules.get(CONSTANTS.MODULE_NAME) as unknown as ForienUnidentifiedItemModuleData;
+  data.socket = socket;
+}
+
+/*
+ * Returns the set socket.
+ * @returns Socket from games module.
+ */
+export function getSocket() {
+  const data = game.modules.get(CONSTANTS.MODULE_NAME) as unknown as ForienUnidentifiedItemModuleData;
+  return data.socket;
+}
+
+Hooks.once('libChangelogsReady', function () {
+  //@ts-ignore
+  libChangelogs.register(
+    CONSTANTS.MODULE_NAME,
+    `
+    - Update typescript to 9
+    - Abbandoned support for 0.8.9
+    - Apply new design pattern to the project
+  `,
+    'minor',
+  );
+});
